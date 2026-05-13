e2f3f28f2b1ede4c2d9cc8c08d2ff4018e7f8f75
Alexander Schmidt <55590992+alexschmander@users.noreply.github.com>
2026-05-13 11:51:50 +0200

    Multi task yolo/fix training weight reuse (#2484)
    * Save crossed model before validation
    
    * Expose training epochs through cli
    
    * Create validation_run_folder to save model inside it
    
    * Remove wrong default project path

--- tools/machine-learning/multi-task-yolo/src/validation/validator.py
+++ tools/machine-learning/multi-task-yolo/src/validation/validator.py
@@ -110,6 +110,13 @@ def validate_hydra_model(
     )
     set_backbone(head_model, backbone, hydra_model.number_of_frozen_modules)
 
+    if not validation_run_folder.exists():
+        validation_run_folder.mkdir(parents=True)
+
+    head_model_yolo_wrapper.save(
+        validation_run_folder / (str(hydra_model) + ".pt")
+    )
+
     head_model_yolo_wrapper.eval()
 
     metrics = head_model_yolo_wrapper.val(
@@ -118,10 +125,6 @@ def validate_hydra_model(
     metrics = cast(DetMetrics, metrics)
 
     save_validation_results(validation_run_folder, metrics.results_dict, config)
-
-    head_model_yolo_wrapper.save(
-        validation_run_folder / (str(hydra_model) + ".pt")
-    )
 
 
 @click.command()
